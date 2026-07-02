beb9a2deeac56c80788ecc0e0296a2a90532fc53
Aarav Sharma <iamcoder18@gmail.com>
2026-07-01 20:05:22 -0600

    feat: add autonomous and localization support
    - Add LimelightHelpers utility for vision/pose estimation
    - Add PathplannerLib vendordep for autonomous path following
    - Add Phoenix6 vendordep for advanced motor control
    - Update Drivetrain for localization integration
    - Update build.gradle with new vendordeps

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,16 +4,29 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.util.LimelightHelpers;
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 
 public class Drivetrain extends SubsystemBase {
@@ -23,6 +36,14 @@ public class Drivetrain extends SubsystemBase {
   private SparkMax backRight;
 
   private DifferentialDrive differentialDrive;
+
+  private final Pigeon2 pigeon = new Pigeon2(20);
+  private DifferentialDriveKinematics m_kinematics =
+      new DifferentialDriveKinematics(Units.inchesToMeters(24.5));
+  private DifferentialDrivePoseEstimator pose =
+      new DifferentialDrivePoseEstimator(m_kinematics, pigeon.getRotation2d(), 0, 0, new Pose2d());
+
+  private final Field2d m_field = new Field2d();
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
@@ -55,6 +76,8 @@ public class Drivetrain extends SubsystemBase {
         backRightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     differentialDrive = new DifferentialDrive(backLeft, backRight);
+
+    SmartDashboard.putData("Field", m_field);
   }
 
   public Command arcadeDrive(DoubleSupplier x, DoubleSupplier y) {
@@ -66,7 +89,46 @@ public class Drivetrain extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    boolean doRejectUpdate = false;
+
+    LimelightHelpers.SetRobotOrientation(
+        "limelight", pose.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+
+    Optional<Alliance> ally = DriverStation.getAlliance();
+
+    LimelightHelpers.PoseEstimate mt2;
+    Pose3d botPose3d;
+    if (ally.isPresent() && ally.get() == Alliance.Red) {
+      mt2 = LimelightHelpers.getBotPoseEstimate_wpiRed_MegaTag2("limelight");
+      botPose3d = LimelightHelpers.getBotPose3d_wpiRed("limelight");
+    } else {
+      mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+      botPose3d = LimelightHelpers.getBotPose3d_wpiBlue("limelight");
+    }
+
+    if (Math.abs(pigeon.getAngularVelocityZDevice().getValueAsDouble()) > 720) {
+      doRejectUpdate = true;
+    }
+
+    if (mt2.tagCount == 0) {
+      doRejectUpdate = true;
+    }
+
+    if (!doRejectUpdate) {
+      pose.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+      pose.addVisionMeasurement(mt2.pose, mt2.timestampSeconds);
+    }
+
+    m_field.setRobotPose(pose.getEstimatedPosition());
+
+    SmartDashboard.putNumber("Drivetrain/X", pose.getEstimatedPosition().getX());
+    SmartDashboard.putNumber("Drivetrain/Y", pose.getEstimatedPosition().getY());
+    SmartDashboard.putNumber("Drivetrain/Z", botPose3d.getZ());
+    SmartDashboard.putNumber("Drivetrain/Yaw", botPose3d.getRotation().getZ());
+    SmartDashboard.putNumber("Drivetrain/Pitch", pigeon.getPitch().getValueAsDouble());
+    SmartDashboard.putNumber("Drivetrain/Roll", pigeon.getRoll().getValueAsDouble());
+    SmartDashboard.putNumber(
+        "Drivetrain/Velocity", pigeon.getAngularVelocityZDevice().getValueAsDouble());
   }
 
   @Override
