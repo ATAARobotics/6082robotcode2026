4d3a05ede028f58e5f3d8a82fab96a87d24dec3e
Aarav <iamcoder18@gmail.com>
2026-07-07 19:35:27 -0600

    Update comment to clarify command mappings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ import frc.robot.subsystems.Drivetrain;
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and command mappings) should be declared here.
+ * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   private final Drivetrain drivetrain = new Drivetrain();
